8d6cf58628984722cb54757757bc53ea1e1ae62e
Judeous <judeous@tutanota.com>
2021-02-05 11:12:35 -0600

    Defined all but T multiplication overload

--- Iterator.h
+++ Iterator.h
@@ -4,15 +4,47 @@ template <typename T>
 class Iterator
 {
 public:
-	Iterator();
-	Iterator(Node<t>* node);
+	Iterator() {};
+	Iterator(Node<T>* node);
 
 	Iterator<T> operator ++();
 	Iterator<T> operator --();
-	const bool operator ==(const Iterator<T>, Iterator);
-	const bool operator !=(const Iterator<T>, Iterator);
-	T operator*();
+	const bool operator ==(const Iterator<T>& iter);
+	const bool operator !=(const Iterator<T>& iter);
+	T operator *();
 
 private:
 	Node<T> current;
 };
+
+template<typename T>
+inline Iterator<T>::Iterator(Node<T>* node)
+{
+	current = node;
+}
+
+template<typename T>
+inline Iterator<T> Iterator<T>::operator++()
+{
+	current = current.next;
+	return Iterator<T>(current);
+}
+
+template<typename T>
+inline Iterator<T> Iterator<T>::operator--()
+{
+	current = current.previous;
+	return Iterator<T>(current);
+}
+
+template<typename T>
+inline const bool Iterator<T>::operator==(const Iterator<T>& iter)
+{
+	return iter.current == current;
+}
+
+template<typename T>
+inline const bool Iterator<T>::operator!=(const Iterator<T>& iter)
+{
+	return iter.current != current;
+}
